43dabd2b8b2ecf3a0f3c08907c921804d2c7f7a3
SohamDesai1 <sohamdesai123@gmail.com>
2024-07-22 19:15:21 +0530

    build: added dependencies and changed main file

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:hisaab/screens/home.dart';
+import 'package:sizer/sizer.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
+  await Supabase.initialize(
+      url: dotenv.env['SUPABASE_PROJECT_URL']!,
+      anonKey: dotenv.env['SUPABASE_API_KEY']!);
   runApp(const MaterialApp(home: MyApp()));
 }
 
@@ -16,6 +23,12 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return const Home();
+    return SafeArea(
+      child: Sizer(
+        builder: (context, orientation, deviceType) {
+          return const Home();
+        },
+      ),
+    );
   }
 }
